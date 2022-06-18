787f2996b143fb6171e15891102b1bb78201333e
Daniel <daniel.shamsudin@outlook.com>
2022-06-19 00:36:52 +0800

    added withdraw cards

--- src/components/Dispense/Dispense.jsx
+++ src/components/Dispense/Dispense.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container, Card, Row } from 'react-bootstrap';
 import Navigation from '../Navigation/Navigation';
+import { db } from '../../firebase';
 
 const Dispense = () => {
+
+	const [data, setData] = useState([]);
+
+	const fetchData = async () => {
+		const items = await db.collection('items').get();
+		const docId = items.docs.map(doc => doc.id);
+		const itemsData = items.docs.map(doc => doc.data());
+		for (let i = 0; i < itemsData.length; i++) {
+			setData(prevData => [...prevData, {
+				id: i + 1,
+				itemName: itemsData[i].itemName,
+				serialNumber: itemsData[i].serialNumber,
+				quantity: itemsData[i].quantity,
+				date: itemsData[i].date,
+				by: itemsData[i].by,
+				remarks: itemsData[i].remarks,
+				docId: docId[i]
+			}]);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, [])
+
 	return (
 		<>
 			<Navigation />
 			<br /><br /><br />
-			<div>
-				<h1>Dispense</h1>
-			</div>
+			<Container>
+				<h1>Withdraw Items</h1>
+				<br />
+				<Container fluid>
+					<Row>
+						{data.map(item => {
+							return (
+								<>
+									<Card key={item.id} style={{ width: '18rem' }}>
+										<Card.Body>
+											<Card.Title>{item.itemName}</Card.Title>
+											<Card.Text>
+												Serial Number: {item.serialNumber}
+												<br />
+												Quantity: {item.quantity}
+												<br />
+												Date: {item.date}
+												<br />
+												By: {item.by}
+												<br />
+												Remarks: {item.remarks}
+											</Card.Text>
+											{/* <Card.Link href={`/dispense/${item.docId}`}>Withdraw</Card.Link> */}
+										</Card.Body>
+									</Card>
+									&nbsp;
+									&nbsp;
+								</>
+							)
+						})}
+					</Row>
+				</Container>
+			</Container>
 		</>
 	)
 }
 
-export default Dispense
+export default Dispense;
