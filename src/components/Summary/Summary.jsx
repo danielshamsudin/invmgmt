49135254acc8f60c4e5d565d319123e885fdb30e
Daniel <daniel.shamsudin@outlook.com>
2022-06-28 09:30:03 +0800

    added date picker on summary page

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -2,23 +2,29 @@ import React, { useState, useEffect } from 'react'
 import Navigation from "../Navigation/Navigation";
 import { Container, Table, Spinner } from "react-bootstrap";
 import { db } from '../../firebase';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 // Fetch data from summary table
 // Cols: Item name, Serial Number, Quantity, Remarks
 // Remarks data is entered from the form when item is dispatched
 const Summary = () => {
 
-	const [summary, setSummary] = useState([]);
-	const [loading, setLoading] = useState(true);
 	let newDate = new Date();
 	let fDate = newDate.toDateString();
+	const [summary, setSummary] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [date, setDate] = useState(new Date());
+	const [dateChoosen, setDateChoosen] = useState(fDate);
+	// setDateChoosen(fDate);
 
 	const fetchData = async () => {
 		const items = await db.collection('transaction').get();
 		const itemsData = items.docs.map(doc => doc.data());
 		console.log(itemsData);
 		for (let i = 0; i < itemsData.length; i++) {
-			if (itemsData[i].date === fDate) {
+			if (itemsData[i].date === dateChoosen) {
 				setSummary(prevData => [...prevData, {
 					id: i + 1,
 					itemName: itemsData[i].itemName,
@@ -33,17 +39,24 @@ const Summary = () => {
 		setLoading(false);
 	}
 
+	const handleDate = (date) => {
+		setDate(date);
+		setDateChoosen(date.toDateString());
+	}
 
 	useEffect(() => {
 		fetchData();
-	}, []);
+		setSummary([]);
+	}, [dateChoosen]);
 
 	return (
 		<>
 			<Navigation />
 			<br /><br /><br />
 			<Container>
-				<h1>Summary of {fDate}</h1>
+				<h1>Summary of {dateChoosen}</h1>
+				<h3>Choose a date:</h3>
+				<DatePicker selected={date} dateFormat="dd/M/yyyy" onChange={date => handleDate(date)} />
 				<br />
 				{loading ? <Spinner animation="border" size="lg" variant="primary"></Spinner> : <Table>
 					<thead>
